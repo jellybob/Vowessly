7122081910c2f205bc9e618317e0ac5bd24bc92b
Jon Wood <jon@blankpad.net>
2010-11-20 14:50:49 +0000

    Do it properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from(Mongo::ConnectionFailure) { render :template => "errors/connection_failure" }
+  rescue_from 'Mongo::ConnectionFailure' do |exception|
+    render :template => "errors/connection_failure"
+  end
 end
