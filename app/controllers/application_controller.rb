9c84cdb016450e5190ae31d897873ece047d4568
Jon Wood <jon@blankpad.net>
2010-11-20 16:33:03 +0000

    Lets stop yakk shaving

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  rescue_from Mongo::ConnectionFailure, :with => :database_failure
-  
-  protected
-    def database_failure
-      render :template => "errors/connetion_failure", :status => 500
-    end
 end
