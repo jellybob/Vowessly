66eace4c640771766f172eaee1ccb513bb20549f
Jon Wood <jon@blankpad.net>
2010-11-21 16:34:15 +0000

    Drop the sidebar until we need it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :disable_sidebar
+
+  protected
+    def disable_sidebar
+      @full_width = true
+    end
 end
