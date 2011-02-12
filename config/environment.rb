c93ab3576ac50cb342df11e75049d1dd1dd05468
Jon Wood <jon@blankpad.net>
2011-02-12 14:11:40 +0000

    Rubygems 1.5.0 changes the YAML parser. It doesn't work.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+# Use Syck for YAML parsing - Psych doesn't do inheritance
+require 'yaml'
+YAML::ENGINE.yamler = 'syck'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
