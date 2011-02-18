276b98fa21996add33c60f294e1b64a6762ab229
Jon Wood <jon@blankpad.net>
2011-02-18 00:28:11 +0000

    Playing with map/reduce

--- app/models/page.rb
+++ app/models/page.rb
@@ -33,4 +33,35 @@ class Page
 
   strip_whitespace :name, :content_type
   embeds_many :facts
+
+  def self.content_types
+    options = { "field.content_type" => "Location" }
+
+    map = <<-MAP
+      function () {
+        location = null;
+        forEach(this.fields) {
+          function (field) {
+            if (field.content_type == "Location") {
+              location = field.value;
+            }
+          }
+        }
+
+        emit(this.name, { content_type: this.content_type, location: location });
+      }
+    MAP
+
+    reduce = <<-REDUCE
+      function (key, values) {
+        return {
+          name: key,
+          content_type: value.content_type,
+          location: value.location
+        }
+      }
+    REDUCE
+
+    mapreduce(map, reduce, options)
+  end
 end
