6dce405a88ce8a9e3252807598eb4fa486bd9962
Jon Wood <jon@blankpad.net>
2011-05-10 11:10:04 +0100

    Add a supervisor username and password

--- config/initializers/basic_auth.rb
+++ config/initializers/basic_auth.rb
@@ -1,5 +1,6 @@
 if ENV.key?("APP_USERNAME") && ENV.key?("APP_PASSWORD")
   Vowessly::Application.config.middleware.insert_after 'Rack::Lock', 'Rack::Auth::Basic' do |username, password|
-    username == ENV["APP_USERNAME"] && password == ENV["APP_PASSWORD"]
+    (username == ENV["APP_USERNAME"] && password == ENV["APP_PASSWORD"]) ||
+    (username == ENV["APP_SUPERVISOR_USERNAME"] && password == ENV["APP_SUPERVISOR_PASSWORD"])
   end
 end
