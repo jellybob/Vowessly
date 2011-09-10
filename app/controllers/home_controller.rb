5166d92ed127f5ff67afde03c1440987cd26ad93
Jon Wood <jon@blankpad.net>
2011-09-10 13:34:45 +0100

    Make sure the flash persists through a home page redirect.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    redirect_to Page.home if Page.count > 0
+    if Page.count > 0
+      flash.keep
+      redirect_to Page.home
+    end
   end
 end
