24e24d8d6a81d4c0045c541e98aa43c80a53717a
Jon Wood <jon@blankpad.net>
2012-09-08 18:17:48 +0100

    Add a comma before page numbers

--- app/models/bibliographer/chapter.rb
+++ app/models/bibliographer/chapter.rb
@@ -1,6 +1,6 @@
 class Bibliographer::Chapter < Bibliographer::Base
   def to_s
-    "#{names}, '#{title}' in |#{book_title}|, ed. by #{editor_names} (#{pub_place}: #{publisher}, #{year}) pp. #{pages}"
+    "#{names}, '#{title}' in |#{book_title}|, ed. by #{editor_names} (#{pub_place}: #{publisher}, #{year}), pp. #{pages}"
   end
 
   def book_title
