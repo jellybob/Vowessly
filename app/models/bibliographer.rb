99ad69e20868bdd3b496429586dd0be78eb2bf04
Jon Wood <jon@blankpad.net>
2012-09-08 17:04:08 +0100

    Hyphenate repeated authors

--- app/models/bibliographer.rb
+++ app/models/bibliographer.rb
@@ -3,18 +3,33 @@ module Bibliographer
     entry_type = page.facts.where(label: "Type").first.value
     case entry_type
     when "Book"
-      Bibliographer::Book.new(page).to_s
+      Bibliographer::Book.new(page)
     when "Chapter in book"
-      Bibliographer::Chapter.new(page).to_s
+      Bibliographer::Chapter.new(page)
     when "Journal article"
-      Bibliographer::JournalArticle.new(page).to_s
+      Bibliographer::JournalArticle.new(page)
     else
-      Bibliographer::Unknown.new(page).to_s
+      Bibliographer::Unknown.new(page)
     end
   end
 
   def self.entries
     pages = Page.where(:content_type => "Reading").all
-    pages.collect { |p| reference_for(p) }.sort
+
+    last_name = nil
+    pages.collect { |p|
+      reference_for(p)
+    }.sort { |a, b|
+      a.to_s <=> b.to_s
+    }.collect { |reference|
+      this_name = reference.names
+      as_string = reference.to_s
+      if this_name == last_name
+        as_string.gsub!(this_name, "----")
+      end
+      last_name = this_name
+
+      as_string
+    }
   end
 end
