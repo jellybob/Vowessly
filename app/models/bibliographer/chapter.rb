f1d63a11f308b224ef29dd8e02356092e7c24f16
Jon Wood <jon@blankpad.net>
2012-09-08 18:01:34 +0100

    Handle editors correctly

--- app/models/bibliographer/chapter.rb
+++ app/models/bibliographer/chapter.rb
@@ -1,6 +1,6 @@
 class Bibliographer::Chapter < Bibliographer::Base
   def to_s
-    "#{names}, '#{title}' in |#{book_title}|, ed. by #{editor_names("Editor")} (#{pub_place}: #{publisher}, #{year})"
+    "#{names}, '#{title}' in |#{book_title}|, ed. by #{editor_names} (#{pub_place}: #{publisher}, #{year})"
   end
 
   def book_title
