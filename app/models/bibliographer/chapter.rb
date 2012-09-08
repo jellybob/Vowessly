7856d4622216b8b2f7025c1ffb0c4ae6ec8fa134
Jon Wood <jon@blankpad.net>
2012-09-08 18:16:11 +0100

    Show page numbers for chapters

--- app/models/bibliographer/chapter.rb
+++ app/models/bibliographer/chapter.rb
@@ -1,9 +1,13 @@
 class Bibliographer::Chapter < Bibliographer::Base
   def to_s
-    "#{names}, '#{title}' in |#{book_title}|, ed. by #{editor_names} (#{pub_place}: #{publisher}, #{year})"
+    "#{names}, '#{title}' in |#{book_title}|, ed. by #{editor_names} (#{pub_place}: #{publisher}, #{year}) pp. #{pages}"
   end
 
   def book_title
     capitalize_title(first_fact("Book title"))
+  end
+
+  def pages
+    first_fact("Pages")
   end
 end
